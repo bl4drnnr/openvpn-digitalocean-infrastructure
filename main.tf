c07aa8d3aac9fcb0c41494e87b501218bfce855a
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-11-06 09:26:50 +0100

    variables upadte

--- main.tf
+++ main.tf
@@ -7,7 +7,25 @@ terraform {
   }
 }
 
-variable "do_token" {}
+variable "do_token" {
+  type    = string
+  default = ""
+}
+
+variable "region" {
+  type    = string
+  default = "fra1"
+}
+
+variable "droplet_count" {
+  type    = number
+  default = 1
+}
+
+variable "droplet_size" {
+  type    = string
+  default = "s-1vcpu-1gb"
+}
 
 provider "digitalocean" {
   token = var.do_token
@@ -19,12 +37,18 @@ data "digitalocean_ssh_key" "openvpn_instance_1_ssh" {
 
 resource "digitalocean_droplet" "web" {
   image  = "ubuntu-20-04-x64"
-  name   = "openvpn-instance-1"
-  size   = "s-1vcpu-1gb"
+  name   = "openvpn-instance-${count.index + 1}"
+  size   = var.droplet_size
+  count  = var.droplet_count
+  region = var.region
   ssh_keys = [data.digitalocean_ssh_key.openvpn_instance_1_ssh.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 output "server_id" {
-  value       = digitalocean_droplet.web.ipv4_address
+  value       = digitalocean_droplet.web.*.ipv4_address
 }
 
