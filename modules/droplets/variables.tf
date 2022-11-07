2ef2dd1d308012664a56e8e5eec5bdcf744d214d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-11-07 07:35:27 +0100

    break down to modules

---
+++ modules/droplets/variables.tf
@@ -0,0 +1,27 @@
+variable "do_token" {
+  description = "DigitalOcean API token"
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "Droplets region"
+  type        = string
+  default     = "fra1"
+}
+
+variable "droplet_size" {
+  description = "Droplet size"
+  type        = string
+  default     = "s-1vcpu-1gb"
+}
+
+variable "droplet_names" {
+  description = "List of droplets names"
+  type        = list(string)
+  default     = [
+    "certificate-authority-server",
+    "openvpn-server",
+    "nextcloud-Server"
+  ]
+}
