4eb7a2fe5c9aa1eeeeb4ac6f4f3de7dc10769173
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-11-09 22:39:42 +0100

    generating ssh keys

--- modules/droplets/droplets.tf
+++ modules/droplets/droplets.tf
@@ -1,5 +1,11 @@
-data "digitalocean_ssh_key" "do_ssh_key" {
-  name = "openvpn-instance-1-ssh"
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
+
+resource "digitalocean_ssh_key" "openvpn_do_ssh" {
+  name       = "OpenVPN-DO-SSH"
+  public_key = tls_private_key.ssh.public_key_openssh
 }
 
 resource "digitalocean_project" "open_vpn" {
@@ -14,7 +20,7 @@ resource "digitalocean_droplet" "droplet_instance" {
   name   = var.droplet_names[count.index]
   size   = var.droplet_size
   region = var.region
-  ssh_keys = [data.digitalocean_ssh_key.do_ssh_key.id]
+  ssh_keys = [digitalocean_ssh_key.openvpn_do_ssh.id]
 
   lifecycle {
     create_before_destroy = true
