e6eee92fc835763ff0ce5ae7096eac9bbc73f970
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-11-11 22:25:30 +0100

    droplet names for inventory

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "openvpn_do_infrastructure_module" {
     source     = "./modules/droplets"
-
+    
     do_token   = var.do_token
 }
--- resources.tf
+++ resources.tf
@@ -1,12 +1,14 @@
 resource "local_file" "servers_ipv4" {
   content   = join("\n", [
-    for s in module.openvpn_do_infrastructure_module.servers_ipv4:
-     format("%q", s)
+    for idx, s in module.openvpn_do_infrastructure_module.servers_ipv4:
+      <<EOT
+${var.droplet_names[idx]} ansible_host=${s} ansible_user=root ansible_ssh_private_key=openvpn_do_ssh.pem
+      EOT
   ])
   filename  = "${path.module}/ansible/inventory.txt"
 }
 
 resource "local_file" "ssh_keys" {
   content   = module.openvpn_do_infrastructure_module.ssh_keys
-  filename  = "${path.module}/ansible/openvpn_do_ssh.key"
+  filename  = "${path.module}/ansible/openvpn_do_ssh.pem"
 }
