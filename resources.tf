8963abdd1c9ce3e2cf209cabd2b9ed4fe221bfdc
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-11-12 11:35:49 +0100

    ansible create user playbook per instance

--- resources.tf
+++ resources.tf
@@ -13,7 +13,7 @@ resource "local_file" "servers_ipv4" {
     "[${var.group_name}]",
     join("\n", [
       for idx, s in module.openvpn_do_infrastructure_module.servers_ipv4:
-      "${var.droplet_names[idx]} ansible_host=${s} ansible_user=root"
+      "${var.droplet_names[idx]} ansible_host=${s} ansible_user=${var.users[idx]}"
     ])
   ]
   )
@@ -26,27 +26,10 @@ resource "local_file" "ssh_keys" {
   file_permission = "0400" 
 }
 
-resource "local_file" "ansible_playbooks_create_users" {
-  content         = join("\n", [
-    "---\n- name: Create non-root users for each droplet\n  hosts: all\n  become: yes\n\n  tasks:",
-    join("\n", [
-      for idx, usr in var.users:
-        <<EOT
-  - name: Create non-root ${usr} user for ${var.droplet_names[idx]}
-    ansible.builtin.user:
-      name: ${usr}
-      group: sudo
-        EOT
-    ])
-  ])
-  filename        = "${path.module}/ansible/create_users.yml"
-  file_permission = "0700"
-}
-
 resource "local_file" "ping_servers" {
   content         = <<EOT
 ---
-- name: Test connection to all servers
+- name:L Test connection to all servers
   hosts: all
   become: yes
 
@@ -58,4 +41,20 @@ resource "local_file" "ping_servers" {
   filename        = "${path.module}/ansible/ping_server.yml"
   file_permission = "0700"
 }
+
+resource "local_file" "ansible_playbooks_create_users" {
+  count           = length(var.users)
+  content         = join("\n", [
+    "---\n- name: Create non-root user for ${var.users[count.index]}\n  hosts: ${module.openvpn_do_infrastructure_module.servers_ipv4[count.index]}\n  become: yes\n\n  tasks:",
+    <<EOT
+  - name: Create non-root user ${var.users[count.index]} for ${var.droplet_names[count.index]}
+    ansible.builtin.user:
+      name: ${var.users[count.index]}
+      group: sudo
+    EOT 
+  ])
+  filename        = "${path.module}/ansible/create_users_${var.droplet_names[count.index]}.yml"
+  file_permission = "0700"
+}
+
 
